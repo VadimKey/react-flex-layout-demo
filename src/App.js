d9bb30c66ef5ebd5ee625f7d0fd4d614637c9bb0
Vadim Key <polecatov@gmail.com>
2021-01-23 20:13:44 +0000

    VK: fixed

--- src/App.js
+++ src/App.js
@@ -1,51 +1,96 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import FlexLayout from "flexlayout-react";
-import layout_model_json from "./model";
 
-<link rel="stylesheet" href="node_modules/flexlayout-react/style/dark.css" />
+import './App.css';
+import '../node_modules/flexlayout-react/style/dark.css';
 
-class Main extends React.Component {
+var json = {
+  global: {},
+  layout: {
+      "type": "row",
+      "weight": 100,
+      "children": [
+          {
+              "type": "tabset",
+              "weight": 50,
+              "selected": 0,
+              "children": [
+                  {
+                      "type": "tab",
+                      "name": "Things to try",
+                      "component": "text",
+                      "config": {"text": "<ul><li>drag tabs</li><li>drag splitters</li><li>double click on tab to rename</li><li>double click on tabstrip to maximize</li><li>use the Add button to add another tab</li></ul>"}
+                  }
+              ]
+          },
+          {
+              "type": "tabset",
+              "weight": 50,
+              "selected": 0,
+              "children": [
+                  {
+                      "type": "tab",
+                      "name": "two",
+                      "component": "text",
+                      "config": {"text": ""}
+                  }
+              ]
+          },
+          {
+            "type": "tabset",
+            "weight": 50,
+            "selected": 0,
+            "children": [
+              {
+                "type":  "tab",
+                "name": "three",
+                "component": "text",
+                "config": {"text": ""}
+              }
+            ]
+          }
+      ]
+  }
+};
+
+class App extends React.Component {
 
   constructor(props) {
-    super(props);
-    this.state = {model: FlexLayout.Model.fromJson(layout_model_json)}; 
-    this.state.model.setOnAllowDrop(this.allowDrop);
-  }
-
-  allowDrop(dragNode, dropInfo) {
-    const dropNode = dropInfo.node;
-
-    // prevent non-border tabs dropping into borders
-    if (dropNode.getType() === "border" 
-      && (dragNode.getParent() == null || dragNode.getParent().getType() !== "border")) {
-        return false;
-    }
-
-    // prevent border tabs dropping into main layout
-    if (dropNode.getType() !== "border"
-      && (dragNode.getParent() != null && dragNode.getParent().getType() === "border") ) {
-        return false;
-    }
-
-    return true;
+      console.log("created");
+      super(props);
+      this.state = {model: FlexLayout.Model.fromJson(json)};
+      this.layoutRef = React.createRef();
   }
 
   factory = (node) => {
-    const component = node.getComponent();
-    if (component === "grid") {
-      return <div className="panel">Panel {node.getName()}</div>;
-    }
+      var component = node.getComponent();
+      if (component === "text") {
+          return <div dangerouslySetInnerHTML={{__html:node.getConfig().text}}/>;
+      }
+  }
+
+  onAdd = (event) => {
+    let layout = this.layoutRef.current;
+    layout.addTabWithDragAndDropIndirect("Add panel<br>(Drag to location)", {
+      component: "text",
+      name: "added",
+      config: {text: "i was added"}
+    }, null);
   }
 
   render() {
-    return(
-      <FlexLayout.Layout model={this.state.model} factory={this.factory}/>
-    );
+      return (
+        <div className="outer">
+          <button onClick={this.onAdd}>Add</button>
+          <div className="inner">
+            <FlexLayout.Layout ref={this.layoutRef}
+                          model={this.state.model}
+                          factory={this.factory}/>
+          </div>
+        </div>
+      );
   }
+}
 
-};
 
-ReactDOM.render(<Main/>, document.getElementById("root"));
-
-export default Main;
+export default App;
